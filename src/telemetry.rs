316b44e85564bbcf9f3c806751687ef9cb4f720f
LilDojd <yawner@pm.me>
2025-10-08 21:48:44 +0400

    set level to info to avoid logging too much

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -3,7 +3,7 @@ use tracing_subscriber::EnvFilter;
 use tracing_subscriber::fmt;
 
 pub fn init() -> Result<()> {
-  let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("debug"));
+  let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
   fmt().with_env_filter(env_filter).with_target(true).init();
   Ok(())
 }
